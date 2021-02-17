32ac9bb02000cf90162e701891874bc41971d570
donkey96 <[noire964@gmail.com]>
2021-02-17 11:19:13 +0900

    そもそもお腹空いていないなら所持金確認などしないように修正

--- 03/pizza/practice/js/app.js
+++ 03/pizza/practice/js/app.js
@@ -1,14 +1,15 @@
-let budget = prompt('所持金を数字で入力してください');
-budget = parseFloat(budget);
-
 let isHungry = confirm('お腹は空いていますか？');
 
 if (true !== isHungry){
   alert('今日はご飯はいいや');
-} else if (budget >= 1500) {
-  alert('ピザを注文しました');
-} else if (budget >= 500) {
-  alert('ポテトを買いました');
 } else {
-  alert('節約、節約...');
+  let budget = prompt('所持金を数字で入力してください');
+  budget = parseFloat(budget);
+  if (budget >= 1500) {
+    alert('ピザを注文しました');
+  } else if (budget >= 500) {
+    alert('ポテトを買いました');
+  } else {
+    alert('節約、節約...');
+  }
 }
