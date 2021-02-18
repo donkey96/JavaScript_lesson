8db0d2d0f833a46b7c3b043daba024895f2c09b0
donkey96 <[noire964@gmail.com]>
2021-02-18 11:21:15 +0900

    さらに要素を追加する

--- 06/change/practice/js/app.js
+++ 06/change/practice/js/app.js
@@ -9,3 +9,9 @@ let first = document.createElement('div');
 first.setAttribute('id', 'first');
 first.innerHTML = '<p>要素を追加</p>';
 practice.insertBefore(first, null);
+
+// さらに要素を追加します
+let second = document.createElement('div');
+second.setAttribute('id', 'second');
+second.innerHTML = '<p>さらに要素を追加</p>';
+practice.insertBefore(second, first);
