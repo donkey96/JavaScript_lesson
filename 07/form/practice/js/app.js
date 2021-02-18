cce957b38c5d39333e0bfc42d0ffe1f91458ea33
donkey96 <[noire964@gmail.com]>
2021-02-18 12:29:02 +0900

    JavaScriptファイルを編集する

--- 07/form/practice/js/app.js
+++ 07/form/practice/js/app.js
@@ -2,7 +2,10 @@
 // フォームの要素を取得
 let button = document.getElementById('button');
 let form = document.getElementById('form');
+let textarea = document.getElementById('textarea');
 
+// 文字数制限
+let maxTextNum = textarea.getAttribute('maxlength');
 /* イベントの処理 **************/
 // お問い合わせボタンを押したとき
 button.addEventListener('click', function() {
