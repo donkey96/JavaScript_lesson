99a9c07e89e2a406e5718697131e53863e126186
donkey96 <[noire964@gmail.com]>
2021-02-18 12:33:53 +0900

    残り文字数を表示するための要素を追加する

--- 07/form/practice/js/app.js
+++ 07/form/practice/js/app.js
@@ -6,6 +6,13 @@ let textarea = document.getElementById('textarea');
 
 // 文字数制限
 let maxTextNum = textarea.getAttribute('maxlength');
+
+// 要素の追加
+// 残り文字数を表示する要素を追加
+let textMessage = document.createElement('div');
+let parent = textarea.parentElement;
+parent.insertBefore(textMessage, textarea);
+
 /* イベントの処理 **************/
 // お問い合わせボタンを押したとき
 button.addEventListener('click', function() {
