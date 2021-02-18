f7015826a826c6d58edd04655917b112fd8242a3
donkey96 <[noire964@gmail.com]>
2021-02-18 13:02:25 +0900

    制限時間が切れたらメッセージを出す

--- 07/form/practice/js/app.js
+++ 07/form/practice/js/app.js
@@ -7,7 +7,7 @@ let textarea = document.getElementById('textarea');
 // 文字数制限
 let maxTextNum = textarea.getAttribute('maxlength');
 // 残り時間（秒）
-let reminingTimeNum = 10;
+let reminingTimeNum = 300;
 
 // 要素の追加
 // 残り文字数を表示する要素を追加
@@ -26,8 +26,12 @@ button.addEventListener('click', function() {
   form.style.display = 'block';
 
   // タイマー処理で残り時間を表示
-  let itmerId = setInterval(function() {
+  let timerId = setInterval(function() {
     timeMessage.innerHTML = '<p>制限時間:' + reminingTimeNum + '秒</p>';
+    if (reminingTimeNum <= 0) {
+      alert('制限時間終了');
+      clearInterval(timerId);
+    }
     reminingTimeNum--;
   }, 1000);
 });
