73597c43fd92e4049cc84e61b45d417c42917e5c
donkey96 <[noire964@gmail.com]>
2021-02-20 12:52:06 +0900

    くじの結果を返すメソッドを作る

--- 08/fortune/practice/js/app.js
+++ 08/fortune/practice/js/app.js
@@ -1,4 +1,12 @@
 // おみくじオブジェクトの定義
 let omikuji = {
-  results: ["大吉", "吉", "中吉", "小吉", "凶"]
+  results: ["大吉", "吉", "中吉", "小吉", "凶"],
+  getResult: function() {
+    let results = this.results;
+    return results[Math.floor(Math.random() * results.length)];
+  }
 }
+
+/* Math.randomで「０〜１」の間でランダムに数字を作る（少数も含む）
+   そこに「* results.length」をどうカッコ内で行うことで「０〜１」→「０〜配列の最大数」となる
+    最後にMath.floorで生成した数字を整数化する。*/
