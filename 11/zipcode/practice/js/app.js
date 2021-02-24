7214a4b374159d4a65b84ac4ecfbdb24bae3cf57
donkey96 <[noire964@gmail.com]>
2021-02-24 17:49:34 +0900

    住所情報をリクエストする(Ajax処理)

--- 11/zipcode/practice/js/app.js
+++ 11/zipcode/practice/js/app.js
@@ -1,6 +1,10 @@
 $(function () {
   $('#btn').on('click', function () {
-    
+   // 入力された郵便番号でWebAPIに住所情報をリクエスト
+    $.ajax({
+      url : "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + $('#zipcode').val(),
+      dataType : 'jsonp',
+    })
   })
 });
 
